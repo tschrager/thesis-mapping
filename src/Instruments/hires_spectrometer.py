fa9450e6debaab6ae20dbf3fa9f912e3ac1a0f53
Terry Filiba <tfiliba@gmail.com>
2012-09-28 13:29:57 -0700

    Adding interface to generate ilp for a high resolution spectrometer

---
+++ src/Instruments/hires_spectrometer.py
@@ -0,0 +1,32 @@
+import abc
+from instrument_base import Instrument_Base
+from CBlocks import *
+from Platforms import *
+
+#defaults
+adc_bits = 8
+
+class Spectrometer(Instrument_Base):
+    
+    def __init__(self,bandwidth,channels,acc_len):
+        self.bandwidth=bandwidth
+        self.channels=channels
+        self.acc_len=acc_len
+        self.adc_bits=adc_bits
+        
+    def generateILP(self):
+        maxhardware = numblocks
+        #loop over all possible platforms and blocks
+        for platorm in Platforms:
+            for currentblock in numblocks:
+                
+        return
+        
+    def generateDataflow(self):
+        self.dataflow = []
+        self.dataflow.append(ADC_CBlock(self.adc_bits,self.bandwidth))
+        self.dataflow.append(PFB_CBlock(self.adc_bits,self.bandwidth))
+        self.dataflow.append(FFT_CBlock(self.adc_bits,self.bandwidth))
+        self.dataflow.append(VAcc_CBlock(self.adc_bits,self.bandwidth,self.acc_len))
+        numblocks = 4
+        return
